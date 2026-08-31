52d4cfc8c04b67e11e6a2bfcfe1f5c6cffb2fab7
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-08-31 23:41:46 +0700

    Added part 3 for Spring Boot Starters Demo

---
+++ Part3_Starters/src/test/java/com/vincenthartono/starters/StartersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenthartono.starters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
