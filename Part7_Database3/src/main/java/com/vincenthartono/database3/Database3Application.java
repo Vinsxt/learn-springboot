3ddfc91ffb9e47d5e86b744879e35eeed2f79447
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-02 15:34:53 +0700

    Created a DatabaseConfig to build JdbcTemplate to interact with Database using SQL with @Configuration and @Bean

---
+++ Part7_Database3/src/main/java/com/vincenthartono/database3/Database3Application.java
@@ -0,0 +1,15 @@
+package com.vincenthartono.database3;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class Database3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Database3Application.class, args);
+	}
+
+}
