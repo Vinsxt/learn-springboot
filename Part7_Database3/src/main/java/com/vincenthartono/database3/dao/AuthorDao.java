426a88f41bd21de8eef80ea50f2127d75846e962
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-05 16:37:30 +0700

    Created find many method, added it to unit test and integration test for AuthorDao

--- Part7_Database3/src/main/java/com/vincenthartono/database3/dao/AuthorDao.java
+++ Part7_Database3/src/main/java/com/vincenthartono/database3/dao/AuthorDao.java
@@ -2,9 +2,12 @@ package com.vincenthartono.database3.dao;
 
 import com.vincenthartono.database3.domain.Author;
 import java.util.Optional;
+import java.util.List;
 
 public interface AuthorDao {
     void create(Author author);
 
     Optional<Author> findOne(long l);
+
+    List<Author> find();
 }
--- Part7_Database3/src/test/java/com/vincenthartono/database3/dao/impl/AuthorDaoImplIntegrationTests.java
+++ Part7_Database3/src/test/java/com/vincenthartono/database3/dao/impl/AuthorDaoImplIntegrationTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,10 +27,25 @@ public class AuthorDaoImplIntegrationTests {
     @Test
     public void testThatAuthorCanBeCreatedAndRecalled() {
 
-        Author author = TestDataUtil.createTestAuthor();
+        Author author = TestDataUtil.createTestAuthorA();
         underTest.create(author);
         Optional<Author> result = underTest.findOne(author.getId());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(author);
+    }
+
+    @Test
+    public void testThatMultipleAuthorsCanBeCreatedAndRecalled(){
+        Author authorA = TestDataUtil.createTestAuthorA();
+        underTest.create(authorA);
+        Author authorB = TestDataUtil.createTestAuthorB();
+        underTest.create(authorB);
+        Author authorC = TestDataUtil.createTestAuthorC();
+        underTest.create(authorC);
+
+        List<Author> result = underTest.find();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(authorA, authorB, authorC);
     }
 }
