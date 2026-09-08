83a7b451aabcdc93e60cd8dfde57c6ba6ed00906
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-08 14:53:48 +0700

    Implemented simple custom query and HQL using @Query for authors

--- Part8_Database4/src/main/java/com/vincenthartono/database4/repositories/AuthorRepository.java
+++ Part8_Database4/src/main/java/com/vincenthartono/database4/repositories/AuthorRepository.java
@@ -1,9 +1,15 @@
 package com.vincenthartono.database4.repositories;
 
 import com.vincenthartono.database4.domain.Author;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a FROM Author a WHERE a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
 }
