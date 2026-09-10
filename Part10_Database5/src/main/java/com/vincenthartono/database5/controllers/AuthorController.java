24e18c3a3e52d1943031b5e1c7c4ea64a0b394ea
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-10 11:57:42 +0700

    Implemented AuthorMapperImpl on AuthorController for create method on presentation layer

--- Part10_Database5/src/main/java/com/vincenthartono/database5/controllers/AuthorController.java
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/controllers/AuthorController.java
@@ -2,6 +2,8 @@ package com.vincenthartono.database5.controllers;
 
 
 import com.vincenthartono.database5.domain.dto.AuthorDto;
+import com.vincenthartono.database5.domain.entities.AuthorEntity;
+import com.vincenthartono.database5.mappers.Mapper;
 import com.vincenthartono.database5.services.AuthorService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +14,17 @@ public class AuthorController {
 
     private AuthorService authorService;
 
-    public AuthorController(AuthorService authorService) {
+    private Mapper<AuthorEntity, AuthorDto> authorMapper;
+
+    public AuthorController(AuthorService authorService, Mapper<AuthorEntity, AuthorDto> authorMapper) {
+        this.authorMapper = authorMapper;
         this.authorService = authorService;
     }
 
     @PostMapping(path = "/authors")
     public AuthorDto createAuthor (@RequestBody AuthorDto author){
-        return authorService.createAuthor(author);
+        AuthorEntity authorEntity = authorMapper.mapFrom(author);
+        AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
+        return authorMapper.MapTo(savedAuthorEntity);
     }
 }
--- Part10_Database5/src/main/java/com/vincenthartono/database5/mappers/impl/AuthorMapperImpl.java
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/mappers/impl/AuthorMapperImpl.java
@@ -4,7 +4,9 @@ import com.vincenthartono.database5.domain.dto.AuthorDto;
 import com.vincenthartono.database5.domain.entities.AuthorEntity;
 import com.vincenthartono.database5.mappers.Mapper;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthorMapperImpl implements Mapper<AuthorEntity, AuthorDto>{
 
     private ModelMapper modelMapper;
