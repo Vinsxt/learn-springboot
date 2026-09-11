00048b9157ddfa28c0b97e0251b138ba8e97e5a1
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-11 14:16:23 +0700

    BookController on progress, switching pc

---
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/controllers/BookController.java
@@ -0,0 +1,11 @@
+package com.vincenthartono.database5.controllers;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    @PutMapping("/books/{isbn}")
+    
+}
