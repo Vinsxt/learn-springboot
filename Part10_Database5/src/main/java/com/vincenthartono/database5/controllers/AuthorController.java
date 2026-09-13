964f1c441b9c808152a07b25cc5536ee920c1725
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-13 14:16:12 +0700

    Implemented find one for authors and tested with Postman

--- Part10_Database5/src/main/java/com/vincenthartono/database5/controllers/AuthorController.java
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/controllers/AuthorController.java
@@ -5,14 +5,13 @@ import com.vincenthartono.database5.domain.dto.AuthorDto;
 import com.vincenthartono.database5.domain.entities.AuthorEntity;
 import com.vincenthartono.database5.mappers.Mapper;
 import com.vincenthartono.database5.services.AuthorService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -40,5 +39,14 @@ public class AuthorController {
         return authors.stream()
                 .map(authorMapper::MapTo)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/authors/{id}")
+    public ResponseEntity<AuthorDto> getAuthor(@PathVariable("id") Long id){
+        Optional<AuthorEntity> foundAuthor = authorService.findOne(id);
+        return foundAuthor.map(authorEntity -> {
+            AuthorDto authorDto = authorMapper.MapTo(authorEntity);
+            return new ResponseEntity<>(authorDto, HttpStatus.OK);
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
--- Part10_Database5/src/main/java/com/vincenthartono/database5/services/AuthorService.java
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/services/AuthorService.java
@@ -3,10 +3,13 @@ package com.vincenthartono.database5.services;
 import com.vincenthartono.database5.domain.entities.AuthorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
 
     AuthorEntity createAuthor(AuthorEntity authorEntity);
 
     List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
