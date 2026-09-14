df8b9df806f38499634fa38e21d1379f12f4959f
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-15 00:54:18 +0700

    Implemented partial update for author

--- Part10_Database5/src/main/java/com/vincenthartono/database5/services/impl/AuthorServiceImpl.java
+++ Part10_Database5/src/main/java/com/vincenthartono/database5/services/impl/AuthorServiceImpl.java
@@ -5,6 +5,7 @@ import com.vincenthartono.database5.repositories.AuthorRepository;
 import com.vincenthartono.database5.services.AuthorService;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -41,5 +42,17 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public boolean isExists(Long id) {
         return authorRepository.existsById(id);
+    }
+
+    @Override
+    public AuthorEntity partialUpdate(Long id, AuthorEntity authorEntity) {
+        authorEntity.setId(id);
+
+        return authorRepository.findById(id).map(existingAuthor -> {
+            Optional.ofNullable(authorEntity.getName()).ifPresent(existingAuthor::setName);
+            Optional.ofNullable(authorEntity.getAge()).ifPresent(existingAuthor::setAge);
+
+            return authorRepository.save(existingAuthor);
+        }).orElseThrow(() -> new RuntimeException("Author does not exist"));
     }
 }
